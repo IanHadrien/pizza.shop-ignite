0e051b81228b5f7dffe461cc1b30c4cec57e7a0c
Ian Hadrien <ianhadrien@gmail.com>
2024-11-02 22:41:24 -0300

    feat: add page orders

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -3,8 +3,8 @@ import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div className="grid min-h-screen grid-cols-2">
-      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+    <div className="grid min-h-screen grid-cols-2 antialiased">
+      <div className="relative flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
         <div className="flex items-center gap-3 text-lg text-foreground">
           <Pizza className="h-5 w-5" />
           <span className="font-semibold">pizza.shop</span>
