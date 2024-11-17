c9936186ea1f834ffa286a3b27037386a86466d6
Ian Hadrien <ianhadrien@gmail.com>
2024-11-17 14:56:31 -0300

    feat: finalizing mocks

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -13,6 +13,12 @@ import { signInMock } from './sign-in-mock'
 import { getProfileMock } from './get-profile-mock'
 import { getManagedRestaurantMock } from './get-managed-restaurant-mock'
 import { updateProfileMock } from './update-profile-mock'
+import { getOrderDetailsMock } from './get-order-details-moks'
+import { getOrdersMock } from './get-order-moks'
+import { dispatchOrderMock } from './dispatch-order-mock'
+import { deliverOrderMock } from './deliver-order-mock'
+import { approveOrderMock } from './approve-order-mock'
+import { cancelOrderMock } from './cancel-order-mock'
 
 export const worker = setupWorker(
   signInMock,
@@ -26,6 +32,12 @@ export const worker = setupWorker(
   getProfileMock,
   getManagedRestaurantMock,
   updateProfileMock,
+  getOrdersMock,
+  getOrderDetailsMock,
+  cancelOrderMock,
+  approveOrderMock,
+  deliverOrderMock,
+  dispatchOrderMock,
 )
 
 export async function enableMSW() {
